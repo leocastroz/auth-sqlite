762997e29696e28bafe95a7bd70c438dd4242402
leocastroz <castroz.dev@gmail.com>
2024-12-27 15:31:05 -0300

    fix: new toml

--- database.js
+++ database.js
@@ -1,21 +1,25 @@
 // database.js
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:'); // ou 'database.db' para persistência
+const path = require('path');
+
+// Use '/data/database.db' to store the SQLite file in the persistent volume
+const dbPath = path.resolve('/data/database.db');
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
-    db.run(`CREATE TABLE users (
+    db.run(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )`);
 
-    db.run(`CREATE TABLE products (
+    db.run(`CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         value REAL
     )`);
 
-    db.run(`CREATE TABLE dispatches (
+    db.run(`CREATE TABLE IF NOT EXISTS dispatches (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         product_id INTEGER,
         FOREIGN KEY (product_id) REFERENCES products(id)
